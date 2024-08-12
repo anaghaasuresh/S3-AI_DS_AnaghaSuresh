52c88224c55c558c71d2f77a89aeae766741e114
anaghaasuresh <anaghakovilakam@gmail.com>
2024-08-12 11:31:33 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+	int a[50],i,j,n,temp;
+	printf("enter the no. of elements:");
+	scanf("%d",&n);
+	printf("enter the elements in array"):
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+		if(a[j]>a[j+1])
+		{
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+		}
+	}
+	printf("the sorted array:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
